16c913063aa36fc70cb911cf8f58abd0ce8dca63
Freddy <Freddy@Freddy-PC>
2016-01-06 11:22:58 -0500

    creando crud

---
+++ ProyectoDishome/ProyectoDishome-ejb/src/main/java/ec/edu/espe/distribuidas/proyectodishome/servicios/ServicioServicio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.distribuidas.proyectodishome.servicios;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Freddy
+ */
+@LocalBean
+@Stateless
+public class ServicioServicio {
+    
+}
